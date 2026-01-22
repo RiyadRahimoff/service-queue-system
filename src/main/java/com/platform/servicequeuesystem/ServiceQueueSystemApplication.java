58505a0880e4a57fd53e5cf3b9f1274b69a5e996
RiyadRahimoff <riyad_rahimov.dev@mail.ru>
2026-01-23 01:28:13 +0400

    package structure

---
+++ src/main/java/com/platform/servicequeuesystem/ServiceQueueSystemApplication.java
@@ -0,0 +1,13 @@
+package com.platform.servicequeuesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceQueueSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceQueueSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/platform/servicequeuesystem/ServiceQueueSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.servicequeuesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceQueueSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
