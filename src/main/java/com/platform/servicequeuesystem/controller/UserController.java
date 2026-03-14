59f0727afe7bdd398e02a364e558311e95568092
RiyadRahimoff <riyad_rahimov.dev@mail.ru>
2026-03-14 16:34:59 +0400

    created UserController

--- src/main/java/com/platform/servicequeuesystem/controller/UserController.java
+++ src/main/java/com/platform/servicequeuesystem/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.platform.servicequeuesystem.controller;
 
-public class UserServiceController {
+import com.platform.servicequeuesystem.dao.entity.UserEntity;
+import com.platform.servicequeuesystem.service.concrete.UserServiceHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    private final UserServiceHandler userServiceHandler;
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createUser(@RequestBody UserEntity userEntity) {
+        userServiceHandler.createUser(userEntity);
+    }
 }
