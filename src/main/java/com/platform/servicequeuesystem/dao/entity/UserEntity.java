f45845358d73fac956e1a3ecc4e3ce82819b5eb8
RiyadRahimoff <riyad_rahimov.dev@mail.ru>
2026-03-14 11:19:56 +0400

    create user entity

---
+++ src/main/java/com/platform/servicequeuesystem/dao/entity/UserEntity.java
@@ -0,0 +1,51 @@
+package com.platform.servicequeuesystem.dao.entity;
+
+import com.platform.servicequeuesystem.model.enums.UserRole;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PRIVATE;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@FieldDefaults(level = PRIVATE)
+@Table(name = "users")
+@Entity
+@Builder
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    Long id;
+
+    String name;
+
+    String email;
+
+    String password;
+
+    UserRole role;
+
+    LocalDateTime createdAt;
+
+    LocalDateTime updatedAt;
+
+
+
+}
---
+++ src/main/java/com/platform/servicequeuesystem/model/enums/UserRole.java
@@ -0,0 +1,4 @@
+package com.platform.servicequeuesystem.model.enums;
+
+public enum UserRole {
+}
